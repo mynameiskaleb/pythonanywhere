bcad06f3b3708e360ce8743df6f7117bb2c48c6e
mynameiskaleb <kalebkeith@pm.me>
2018-04-09 18:04:09 -0400

    Added a main site file, an __init__, a routes file, and my site txt file.

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  9 10:14:43 2018
+
+@author: kaleb
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+from app import routes
---
+++ site.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  9 17:55:23 2018
+
+@author: kaleb
+"""
+
+from app import app
